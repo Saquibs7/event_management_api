3cb96228133bb10a9c84dda701c0ee430ddb309c
saquibs7 <saquibhussain1289@gmail.com>
2025-07-30 22:46:01 +0530

    directory setup

---
+++ template.sh
@@ -0,0 +1,17 @@
+mkdir -p controllers
+mkdir -p routes
+mkdir -p middlewares
+mkdir -p db
+mkdir -p services
+mkdir -p utils
+touch db/index.js
+touch controllers/eventController.js
+touch routes/events.js
+touch routes/users.js
+touch controllers/userController.js
+touch services/eventService.js
+touch services/userService.js
+touch middlewares/validate.js
+touch utils/sortEvents.js
+touch .env
+echo "directory and files created succesfully"
